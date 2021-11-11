84f0b4405e80814136c09503fb5d75b8c7df12f7
Joe Kaikaty <joe.kaikaty@gmail.com>
2021-11-11 13:56:14 +0200

    Delete test directory

--- test/test.js
+++
@@ -1,9 +0,0 @@
-for (let i = 0; i < 100; i++) {
-   if (i % 3 === 0) {
-       console.log('Fizz')
-   }else if (i % 5 === 0) {
-       console.log('Buzz')
-   }else if (i % 3 && i % 5) {
-       console.log('FizzBuzz')
-   }
-}
